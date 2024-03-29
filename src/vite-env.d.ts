353ef957fb9755e5a6f6804d453bc8aa404f28e7
Alex S <habahabahaba@outlook.com>
2024-03-29 05:42:55 +0400

    hiding_key_2

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string;
-  readonly GOOGLE_API_KEY: string;
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
